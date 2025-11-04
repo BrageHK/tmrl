5c2fa96f6c8c8bbc62d8c71da7d26a9796338371
BrageNTNU <bragehk@stud.ntnu.no>
2025-11-04 19:47:20 +0100

    bruh

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='tmrl',
-    version="69",
-    description='Network-based framework for real-time robot learning',
-    long_description_content_type='text/markdown',
-    keywords='reinforcement learning, robot learning, trackmania, self driving, roborace',
-    license='MIT',
-    install_requires=install_req,
-    extras_require={},
-    scripts=[],
-    packages=find_packages(exclude=("tests", )))
+    license='MIT'
+    )
